b832b7d3bd27656d31c3d0f1debec5d075a6cd1f
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-17 16:12:32 -0400

    feat:Successfully pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const newString = string.split("");
+    newString.reverse();
+    return newString.join("");
 };
 
 // Do not edit below this line
