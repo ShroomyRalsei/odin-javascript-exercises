05a2521023105f90e40d1b2e92af77f8c902b847
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-17 18:08:15 -0400

    feat:Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function(array, number) {
-    const undesiredElementIndex = array.indexOf(number);
-    array.splice(undesiredElementIndex, 1);
+const removeFromArray = function(array, ...undesiredElements) {
+    for (i = 0; i < undesiredElements.length; i++) {
+        const undesiredElementIndex = array.indexOf(undesiredElements[i]);
+        if(undesiredElements[i] === array[undesiredElementIndex]) {
+            array.splice(undesiredElementIndex, 1);
+        }
+    }
     return array;
 };
 
