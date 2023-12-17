689ec96fe98ecda67eba26948ed95367b3caccf5
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-17 15:49:18 -0400

    fix:Make function return error if num is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,14 @@
 const repeatString = function(string = "", num = 0) {
     let finalText = '';
-    for (i = 0; i < num; i++) {
-        finalText += string;
+    if (num < 0) {
+        return "ERROR";
     }
-    return finalText;
+    else {
+        for (i = 0; i < num; i++) {
+            finalText += string;
+        }
+        return finalText;
+    }
 };
 
 // Do not edit below this line
