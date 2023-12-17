9f7d9c371d629dca2cba2722210f72bddb41468e
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-17 15:46:03 -0400

    feat:Pass first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string = "", num = 0) {
+    let finalText = '';
+    for (i = 0; i < num; i++) {
+        finalText += string;
+    }
+    return finalText;
 };
 
 // Do not edit below this line
