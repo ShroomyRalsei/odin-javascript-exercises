ff5dbbb7f555d47954a219f5e800437ee8bf8a74
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-17 20:04:21 -0400

    feat:Pass first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celsiusValue) {
+  const celsiusToFarenheit = (celsiusValue - 32) * (5/9);
+  return Math.round(celsiusToFarenheit,1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(farenheitValue) {
+  const farenheitToCelsius = (farenheitValue * (9/5)+ 32);
+  return Math.round(farenheitToCelsius, 1);
 };
 
 // Do not edit below this line
