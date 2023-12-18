5086b48f5016a15b12eb27c3f81e4e92a95826a1
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2023-12-17 20:07:57 -0400

    fix:Fix return values

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(celsiusValue) {
   const celsiusToFarenheit = (celsiusValue - 32) * (5/9);
-  return Math.round(celsiusToFarenheit,1);
+  return Math.round(celsiusToFarenheit * 10) / 10;
 };
 
 const convertToFahrenheit = function(farenheitValue) {
   const farenheitToCelsius = (farenheitValue * (9/5)+ 32);
-  return Math.round(farenheitToCelsius, 1);
+  return Math.round(farenheitToCelsius * 10) / 10;
 };
 
 // Do not edit below this line
